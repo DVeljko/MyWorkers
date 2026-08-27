9d905798e3701361958353d8864e090dda3771a7
Veljko Dimitrijevic <dveljko3@gmail.com>
2026-08-27 14:18:15 +0200

    Add employee creation endpoint

--- employee_service/app.py
+++ employee_service/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from models import db, Employee
+from datetime import date
 
 app = Flask(__name__)
 
@@ -23,6 +24,24 @@ def single_employee(employee_id):
     employee = db.get_or_404(Employee, employee_id)
     return jsonify(employee.to_dict())
 
+@app.route("/employees", methods=['POST'])
+def add_employee():
+    employee = request.get_json()
+    new_employee = Employee(
+        first_name = employee['first_name'],
+        last_name = employee['last_name'],
+        email = employee['email'],
+        phone = employee['phone'],
+        position = employee['position'],
+        hire_date = date.fromisoformat(employee['hire_date']),
+        salary = employee['salary'],
+        status = employee['status'],
+        department_id = employee['department_id']
+    )
+
+    db.session.add(new_employee)
+    db.session.commit()
+    return jsonify(new_employee.to_dict()), 201
 
 
 
