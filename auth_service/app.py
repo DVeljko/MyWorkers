e980615520e6745f0dae8eac1d62c96fd756a456
Veljko Dimitrijevic <dveljko3@gmail.com>
2026-08-31 22:44:22 +0200

    Add user model and auth service setup

--- auth_service/app.py
+++ auth_service/app.py
@@ -1,8 +1,41 @@
-from flask import Flask
+from flask import Flask, abort
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User, db
+from dotenv import load_dotenv
+import os
+from functools import wraps
+
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, user_id)
+
+def admin_required(f):
+    @wraps(f)
+
+    def check_admin(*args, **kwargs):
+        if not current_user.is_authenticated or current_user.role != "admin":
+            abort(403)
 
 
+        return f(*args,**kwargs)
+
+    return check_admin
 
 
 
---
+++ auth_service/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_login import UserMixin
+
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    role: Mapped[str] = mapped_column(nullable=False)
